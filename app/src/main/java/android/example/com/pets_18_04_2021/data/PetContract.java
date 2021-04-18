ee4d9549454bbf54ef226b73124ec982407db62a
swapan <swapan.d83@gmail.com>
2021-04-19 00:08:29 +0600

    Add constants for pet content URIs

--- app/src/main/java/android/example/com/pets_18_04_2021/data/PetContract.java
+++ app/src/main/java/android/example/com/pets_18_04_2021/data/PetContract.java
@@ -1,5 +1,6 @@
 package android.example.com.pets_18_04_2021.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -11,10 +12,35 @@ public class PetContract {
     private PetContract() {}
 
     /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "android.example.com.pets_18_04_2021";
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_PETS = "pets_18_04_2021";
+
+
+    /**
      * Inner class that defines constant values for the pets database table.
      * Each entry in the table represents a single pet.
      */
     public static final class PetEntry implements BaseColumns {
+        /** The content URI to access the pet data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+        
         /** Name of database table for pets */
         public final static String TABLE_NAME = "pets";
 
