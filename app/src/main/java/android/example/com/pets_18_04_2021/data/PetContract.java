c2ea41cc7b82253723b906e2b563a449b364a473
swapan <swapan.d83@gmail.com>
2021-04-19 00:39:44 +0600

    Add UriMatcher to ContentProvider

--- app/src/main/java/android/example/com/pets_18_04_2021/data/PetContract.java
+++ app/src/main/java/android/example/com/pets_18_04_2021/data/PetContract.java
@@ -40,7 +40,7 @@ public class PetContract {
     public static final class PetEntry implements BaseColumns {
         /** The content URI to access the pet data in the provider */
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
-        
+
         /** Name of database table for pets */
         public final static String TABLE_NAME = "pets";
 
